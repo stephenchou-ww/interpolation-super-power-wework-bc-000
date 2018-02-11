19f32249952fc103b820f3962cd2a5ab86112c4e
stephenchou-ww <stephen.chou@wework.com>
2018-02-11 22:35:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 
 
 def display_rainbow(rainbow)
-  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}. G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]}\n"
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}. G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]}" + "\n"
 end
 
 
