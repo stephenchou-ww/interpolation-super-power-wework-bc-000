79a98685bf7d97f199c9de5d3963406b51537d64
stephenchou-ww <stephen.chou@wework.com>
2018-02-11 22:29:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,11 @@
 # Write your #display_rainbow method here
 
 
-display_rainbow()
+def display_rainbow(rainbow[])
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}. G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]}"
+  
+end
+
+
 
 
